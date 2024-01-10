b446452ee50aa533d82b9cdb9c09435296175fae
Betul001 <betulmemduhoglu8@gmail.com>
2024-01-10 11:59:39 +0300

    first commit

--- randomresimler.py
+++ randomresimler.py
@@ -3,12 +3,12 @@ import requests
 reqUrl = "https://picsum.photos/200/300"
 
 headersList = {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+ "Accept": "*/*",
+ "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
 }
 
 payload = ""
 
-response = requests.get(reqUrl, data=payload, headers=headersList)
+response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
 
 print(response.text)
